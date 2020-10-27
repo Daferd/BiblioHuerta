a37337cbf3bc81936f37137b81154cb36b6784db
Dario Arevalo <daferd19@gmail.com>
2020-10-27 17:04:01 -0500

    Initial commit

---
+++ app/src/main/java/com/daferarevalo/bibliohuerta/RegisterActivity.kt
@@ -0,0 +1,21 @@
+package com.daferarevalo.bibliohuerta
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_register.*
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+        regLoginButton.setOnClickListener {
+            onBackPressed()
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        overridePendingTransition(R.anim.slide_from_left,R.anim.slide_to_right)
+    }
+}
